f4aebfc3ff59da70401707970a8c6d0768dc1d2a
Lin Zhang <lin.zhang.ux@gmail.com>
2018-08-10 20:45:34 -0400

    try to fix https issue

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
 
     $(this).addClass('is-loading').text("Loading...");
 
-    $.get('/blog/page/' + nextPage, function(data) {
+    $.get('/blog/page/' + nextPage + "/", function(data) {
       var htmlData = $.parseHTML(data);
       var $articles = $(htmlData).find('article');
 
